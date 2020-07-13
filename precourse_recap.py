45c65f4ee683cef9cbf63e31689c58a3b8c52921
Jonny Reading <jonnyreading@gmail.com>
2020-07-13 17:51:45 +0100

    Added psuedocode

---
+++ precourse_recap.py
@@ -0,0 +1,36 @@
+"""
+simple game using conditionals in python
+    pokemon battle!
+"""
+# explain the game to the user
+print ("Pikachu wants to battle!")
+
+pikachu_health = 50
+charmander_health = 50
+
+pikachu_thunderbolt_pp = 1
+pikachu_shockwave_pp = 1
+
+charmander_inferno_pp = 1
+charmander_flamethrower_pp = 1
+
+
+# ask which move to play
+    # damage pikachu based on move
+    # move pp -= 1
+
+#computer has a turn
+    # damage charmander based on move
+    # move pp -= 1
+
+# player has another turn
+    # check pp
+        # skip go if pp == 0
+        # computer plays again
+        # charmander fainted!
+    # else play move
+        # check if health is all gone
+            # you won!
+
+
+
